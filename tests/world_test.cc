07c5647324f1b918e8dda49acfe983b9baa7c1f5
v4m3rrr <igorurbanowicz@gmail.com>
2025-11-15 19:26:57 +0100

    Win changes

--- tests/world_test.cc
+++ tests/world_test.cc
@@ -3,6 +3,20 @@
 using namespace boink;
 using namespace Eigen;
 
+void test_heap_overflow() {
+    // Allocate an array of 10 integers (40 bytes) on the heap
+    int* data = new int[10];
+
+    // GOOD ACCESS: Accessing index 5 is safe
+    data[5] = 100;
+
+    // BAD ACCESS: Accessing index 10 (the 11th element), which is past the end of the allocated block.
+    // This is a one-byte out-of-bounds access.
+    data[10] = 200; // <--- ASan should crash here!
+
+    delete[] data;
+}
+
 int main()
 {
   CarModel car_model;
@@ -17,4 +31,6 @@ int main()
 
   world.start(0.0);
   world.update(10.0);
+
+  test_heap_overflow();
 }
