85f76f004b38908ac00bfa9c7dfad040e1054dfe
v4m3rrr <igorurbanowicz@gmail.com>
2025-11-29 14:59:56 +0100

    Init

---
+++ include/boink/systems/render_system.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "boink/components/component_manager.h"
+
+#include "boink/components/transform.h"
+#include "boink/components/car_model.h"
+
+namespace boink
+{
+  /**
+   * @brief Simulates movement of a vehicle
+   *
+   */
+  class MovementSystem
+  {
+  public:
+    template <typename TupleStaticComponents_, typename TupleComponents_>
+    void Setup(
+        ComponentManager<TupleStaticComponents_,TupleComponents_>& component_manager,
+        double dt)
+    {
+        auto view = component_manager.
+          template getComponentView<Transform>();
+        auto static_comps=component_manager.
+          template getStaticComponentView<CarModel>();
+        const auto& model = std::get<CarModel&>(static_comps);
+        view.forEach(
+            [&](Transform& trans)
+            {
+            }
+        );
+    }
+    template <typename TupleStaticComponents_, typename TupleComponents_>
+    void Update(
+      ComponentManager<TupleStaticComponents_,TupleComponents_>& component_manager,
+      double dt)
+    {
+      auto view=component_manager
+        .template getComponentView<Transform>();
+
+      auto static_comps=component_manager.
+        template getStaticComponentView<CarModel>();
+      const auto& model = std::get<CarModel&>(static_comps);
+
+      view.forEach(
+        [&, dt](
+          Transform& trans
+        )
+        {
+        }
+      );
+    }
+
+    template <typename TupleStaticComponents_, typename TupleComponents_>
+    void UpdateWhenPossible(
+      ComponentManager<TupleStaticComponents_,TupleComponents_>& component_manager,
+      double dt)
+    {
+
+    }
+  private:
+
+  };
+}
